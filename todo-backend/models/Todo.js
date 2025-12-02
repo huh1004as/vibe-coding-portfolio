fe5e77cc28bafae856b0dafb32a0512f0c3ae873
Lucas <huh1004as@naver.com>
2025-12-02 21:29:07 +0900

    Initial commit: VIBE-CODING portfolio projects

---
+++ todo-backend/models/Todo.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const todoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    trim: true,
+    default: ''
+  }
+}, {
+  timestamps: true // createdAt, updatedAt 자동 생성
+});
+
+const Todo = mongoose.model('Todo', todoSchema);
+
+export default Todo;
+
